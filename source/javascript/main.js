bc18d2d067ead6c075605a4971a7ea91fcb4989f
Frank Gjertsen <frank.gjertsen@gmail.com>
2015-04-30 23:57:36 +0200

    Style space between section-row's, section-title, sms-campaign

--- source/javascript/main.js
+++ source/javascript/main.js
@@ -4,3 +4,27 @@
  * Fluid videos using fitvids
  */
 $('.js-fitvid').fitVids();
+
+
+// Accordion
+var acc = $('.js-accordion'),
+    accChild = acc.children('li'),
+    accGrandChild = accChild.children('ul');
+
+// accGrandChild.hide();
+var text = accChild[0].childNodes[0],
+    textNode = document.createTextNode(text.nodeValue),
+    anchor = document.createElement('a');
+
+anchor.setAttribute('href', '');
+anchor.appendChild(textNode);
+
+console.log(anchor);
+
+// for(var i = 0, j = accChild.length; i < j; i++ ) {
+//   console.log(accChild[i]);
+// }
+
+accChild.on('click', function() {
+  $(this).children('ul').slideToggle(300);
+});
